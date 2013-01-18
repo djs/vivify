8db841d0a1ef5f4f91bc494fbe8aea7438c745c0
Dan Savilonis <djs@n-cube.org>
2013-01-17 22:21:01 -0500

    enlighten conemu and clink, and allow individual enlightenment

--- enlighten.py
+++ enlighten.py
@@ -1,15 +1,23 @@
+import argparse
 from bs4 import BeautifulSoup
 import os.path
+import platform
+import re
+import shutil
 import subprocess
+import sys
 import urllib2
 import zipfile
 
-def main():
-    sevenzip()
-    ruby('exe')
-    ruby('sfx')
 
+def platform_check():
+    if platform.machine() != 'AMD64':
+        print >> sys.stderr, "%s architecture not supported"
+        sys.exit(1)
 
+    if int(platform.release()) < 7:
+        print >> sys.stderr, "Only modern Windows are supported"
+        sys.exit(1)
 
 def ruby(kit):
     f = urllib2.urlopen('http://rubyinstaller.org/downloads/')
@@ -48,7 +56,91 @@ def sevenzip():
     with zipfile.ZipFile('7za920.zip', 'r') as myzip:
         myzip.extract('7za.exe', os.path.expandvars('$MY_USER_BIN'))
 
+def conemu():
+    f = urllib2.urlopen('http://code.google.com/p/conemu-maximus5/downloads/list')
+    soup = BeautifulSoup(f)
+    f.close()
 
+
+    ext = soup.find('table', id='resultstable').td.a['href']
+    url = 'http://code.google.com' + ext
+
+
+    f = urllib2.urlopen('http://code.google.com/p/conemu-maximus5/downloads/list')
+    soup = BeautifulSoup(f)
+    f.close()
+
+    ext = soup.find('a', href=re.compile('.exe$'))['href']
+    installer = 'http:' + ext
+
+    f = urllib2.urlopen(installer)
+    with open(os.path.basename(installer), "wb") as local_installer:
+        local_installer.write(f.read())
+
+    f.close()
+
+    subprocess.check_call([os.path.basename(installer), '/p:x64',
+        '/passive'])
+
+
+def clink():
+    installer = 'http://download.microsoft.com/download/A/8/0/A80747C3-41BD-45DF-B505-E9710D2744E0/vcredist_x64.exe'
+    f = urllib2.urlopen(installer)
+    with open(os.path.basename(installer), "wb") as local_installer:
+        local_installer.write(f.read())
+    f.close()
+
+    subprocess.check_call([os.path.basename(installer), '/passive'])
+    f = urllib2.urlopen('https://code.google.com/p/clink/downloads/list')
+    soup = BeautifulSoup(f)
+    f.close()
+
+
+    ext = soup.find('table', id='resultstable').td.a['href']
+    url = 'http://code.google.com' + ext
+
+
+    f = urllib2.urlopen('https://code.google.com/p/clink/downloads/list')
+    soup = BeautifulSoup(f)
+    f.close()
+
+    ext = soup.find('a', href=re.compile('.exe$'))['href']
+    installer = 'http:' + ext
+
+    f = urllib2.urlopen(installer)
+    with open(os.path.basename(installer), "wb") as local_installer:
+        local_installer.write(f.read())
+
+    f.close()
+
+    subprocess.check_call([os.path.basename(installer), '/S'])
+
+    directory = os.path.join('C:\\Program Files (x86)\\clink', os.walk('C:\\Program Files (x86)\\clink').next()[1][0])
+    files = os.listdir(os.path.join(directory, directory))
+    for filename in files:
+        fullname = os.path.join(directory, filename)
+        if(os.path.isfile(fullname)):
+            print fullname
+            shutil.copy(fullname, 'C:\\Program Files\\ConEmu\\ConEmu\\clink')
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("item")
+    args = parser.parse_args()
+
+    platform_check()
+
+    if args.item == 'all':
+        sevenzip()
+        ruby('exe')
+        ruby('sfx')
+        conemu()
+        clink()
+    else:
+        method = globals()[args.item]
+        method()
 
 if __name__ == '__main__':
     main()
